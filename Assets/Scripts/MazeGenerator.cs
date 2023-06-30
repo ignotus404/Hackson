2af49721a3ea1d8b0545fa86c18836c71d1e0d90
AAA_kosen <s2002251@sendai-nct.jp>
2023-06-30 10:31:14 +0900

    迷路スクリプト完成

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class MazeGenerator : MonoBehaviour
 {
-    [Header("壁をまとめたオブジェクト"),SerializeField]GameObject MazeGround; //迷路の壁全部をまとめるオブジェクト
+    [Header("壁をまとめたオブジェクト")]
+    [SerializeField] GameObject MazeGround; //迷路の壁全部をまとめるオブジェクト
 
     int MazeX;
     int MazeY;
-    int receiveNum = 0; //棒を倒す方向を決める乱数
-    bool Complete = false; //迷路生成完了フラグ
-    GameObject[,] MazeObject;
     int[,] Maze; //迷路のデータを格納する配列
-   
-    
+
     //マップ生成
     void Awake()
     {
@@ -21,52 +18,39 @@ public class MazeGenerator : MonoBehaviour
         MazeY = MazeGround.transform.GetChild(0).childCount;
 
         GameObject[,] MazeObject = new GameObject[MazeX, MazeY];
-        int[,] Maze = new int[MazeX, MazeY]; //迷路のデータを格納する配列
 
-        Debug.Log(Maze.Length);
+        Debug.Log(MazeX);
+        Debug.Log(MazeY);
+
+        for (int i = 0; i < MazeX; i++)
+        {
+            for (int j = 0; j < MazeY; j++)
+            {
+                MazeObject[i, j] = MazeGround.transform.GetChild(i).GetChild(j).gameObject;
+            }
+        }
+
+        Maze = new int[MazeX, MazeY];
+        InitializeMaze();
+
         Random.InitState(System.DateTime.Now.Millisecond); //乱数の初期化
 
-        //迷路の壁をまとめるオブジェクトを取得
-        for(int i = 0; i < MazeX; i++)
-        {
-            for(int j = 0; j < MazeY; j++)
-            {
-                MazeObject[i,j] = MazeGround.transform.GetChild(i).GetChild(j).gameObject;
-            }
-        }
-
-        //Maze初期化
-        for(int i = 0; i < MazeX; i++)
-        {
-            for(int j = 0; j < MazeY; j++)
-            {
-                Maze[i, j] = 0; //全てのマスを道にする
-            }
-        }
-
         //倒すための棒をつくる
-        for(int i = 0; i < MazeX; i++)
+        for (int i = 1; i < MazeX; i += 2)
         {
-            for(int j = 0; j < MazeY; j++)
+            for (int j = 1; j < MazeY; j += 2)
             {
-                if((i%2 == 1) && (j%2 == 1)) 
-                {
-                    Maze[i, j] = 1; //奇数のマスに棒を倒す
-                    while(!Complete)
-                    {
-                        receiveNum = Random.Range(1, 13) % 4; //1~4の乱数を生成
-                        Complete = MazeCreate(i, j, receiveNum, Maze); //棒を倒す
-                    }
-                }
+                Maze[i, j] = 1; //奇数のマスに棒を倒す
+                CarvePath(i, j);
             }
-        }   
-        
+        }
+
         //配列どおりにマップを生成する
-        for(int i = 0; i < MazeX; i++)
+        for (int i = 0; i < MazeX; i++)
         {
-            for(int j = 0; j < MazeY; j++)
+            for (int j = 0; j < MazeY; j++)
             {
-                if(Maze[i, j] == 1)
+                if (Maze[i, j] == 1)
                 {
                     MazeObject[i, j].SetActive(true);
                 }
@@ -78,61 +62,68 @@ public class MazeGenerator : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void InitializeMaze()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    bool MazeCreate(int x, int y,int receiveNum,int[,] Maze)
-    {
-        Debug.Log(Maze.Length);
-        switch (receiveNum)
+        for (int i = 0; i < MazeX; i++)
         {
-            case 0:
-                //上方向に棒を倒す
-                if(y != 1 && Maze[x, y-1] == 0)
-                {
-                    Maze[x, y-1] = 1;
-                    return true;
-                }
-                else return false;
-
-            case 1:
-                //下方向に棒を倒す
-                if(Maze[x, y+1] == 0)
-                {
-                    Maze[x, y+1] = 1;
-                    return true;
-                }
-                else return false;
-
-            case 2:
-                //左方向に棒を倒す
-                if(x != 1 && Maze[x-1, y] == 0 )
-                {
-                    Maze[x-1, y] = 1;
-                    return true;
-                }
-                else return false;
-
-            case 3:
-                //右方向に棒を倒す
-                if(Maze[x+1, y] == 0)
-                {
-                    Maze[x+1, y] = 1;
-                    return true;
-                }
-                else return false;
-
-            default:
-                return false;
+            for (int j = 0; j < MazeY; j++)
+            {
+                Maze[i, j] = 0; //全てのマスを道にする
+            }
         }
+    }
+
+    void CarvePath(int x, int y)
+    {
+        int[] directions = { 0, 1, 2, 3 };
+        ShuffleArray(directions);
+
+        foreach (int direction in directions)
+        {
+            int nextX = x;
+            int nextY = y;
+
+            switch (direction)
+            {
+                case 0: // 上
+                    //1行目のみ上に道を作る
+                    if(y == 1)
+                    {
+                        nextY -= 1;
+                    }
+                    break;
+                case 1: // 下
+                    nextY += 1;
+                    break;
+                case 2: // 左
+                    nextX -= 1;
+                    break;
+                case 3: // 右
+                    nextX += 1;
+                    break;
+            }
+
+            if (IsInBounds(nextX, nextY) && Maze[nextX, nextY] == 0)
+            {
+                Maze[nextX, nextY] = 1; //道にする
+                return;
+            }
+        }
+    }
+
+    void ShuffleArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int temp = array[i];
+            int randomIndex = Random.Range(i, array.Length);
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
+        }
+    }
+
+    bool IsInBounds(int x, int y)
+    {
+        return x >= 0 && x < MazeX && y >= 0 && y < MazeY;
     }
 }
